b7eb0e833c11f9dc0c97c5174965b13196eb844a
Naman Jain <naman@Namans-Macbook.local>
2024-07-02 01:53:23 +0530

    Finishing Header of the website, Added background assets and added a few fonts

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Naman Jain'),
     );
   }
 }
@@ -77,49 +61,92 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.only(top: 50),
+        margin: EdgeInsets.symmetric(horizontal: 122),
+        decoration: BoxDecoration(
+            border: Border(left: BorderSide(width: 3)),
+            image: DecorationImage(
+                image: AssetImage("assets/dots.png"),
+                repeat: ImageRepeat.repeat)),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.475,
+                ),
+                Image.asset(
+                  "assets/Icon_before_name.png",
+                  height: 45,
+                  width: 45,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Naman",
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontFamily: "Handlee",
+                      fontWeight: FontWeight.w600),
+                ),
+                Spacer(),
+                Text(
+                  "Portfolio",
+                  style: TextStyle(fontFamily: "Inter", fontSize: 24),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                TextWithHighlight(),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TextWithHighlight extends StatelessWidget {
+  final String text;
+  final Color color;
+  TextWithHighlight(
+      {this.text = "Hire Me", this.color = const Color(0xffFFC9F0), super.key});
+
+  final tp = TextPainter(
+      text: TextSpan(
+          text: 'Hire Me', style: TextStyle(fontFamily: "Inter", fontSize: 24)),
+      textDirection: TextDirection.ltr);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: color,
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.transparent,
+                Colors.transparent,
+                color,
+                color,
+                Colors.transparent
+              ],
+              stops: [
+                0,
+                0.5,
+                0.5,
+                0.8,
+                0.8
+              ])),
+      child: Text(
+        text,
+        style: TextStyle(fontFamily: "Inter", fontSize: 24),
+      ),
     );
   }
 }
